3da22c09108f7b3a06a1a74d9c81ba07e00f11bb
kate_lulu <lukoszek.katarzyna@gmail.com>
2022-02-15 10:59:39 +0100

    2

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -20,20 +20,21 @@ export default function AboutMe() {
               <img src={about} alt="About" className="about-img" />
             </Carousel.Item>
             <Carousel.Item>
-              <img src={laptop} alt="About" className="about-img" />
-              <Carousel.Caption>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
+              <img src={laptop} alt="About" className="about-img2" />
+              <Carousel.Caption className="about-textBox">
+                <p className="about-text">
+                  Hi! I'm Katarzyna, Junior Frontend Developer. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <img src={office} alt="About" className="about-img" />
-              <Carousel.Caption>
-                <p>
+              <img src={office} alt="About" className="about-img2" />
+              <Carousel.Caption className="about-textBox">
+                <p className="about-text">
                   Duis aute irure dolor in reprehenderit in voluptate velit esse
                   cillum dolore eu fugiat nulla pariatur.
                 </p>
@@ -52,20 +53,3 @@ export default function AboutMe() {
     </section>
   )
 }
-
-/*----------------------------------------------------------------
-
-        <img src={about} alt="About" className="about-img" />
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <p>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
-          </p>
-
-          */
